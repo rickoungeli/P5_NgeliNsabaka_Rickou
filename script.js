e58eacfac42b707062bca3704957ca3a20128fa1
Rickou NGELI NSABAKA <rickoungeli@gmail.com>
2021-05-27 08:10:03 +0200

    suite contruction page 2

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-const conteneurProduits = document.getElementById('conteneurProduits')
+// -----------P A G E   1 ---------------//
+let conteneurProduits = document.getElementById('conteneurProduits')
 const searchZone = document.getElementById('searchzone')
 let searchTerm = ''
-//fetch ("https://rickoungeli.github.io/ngelinsabakarickou_5_18052021/api/teddies")
 let datas
+//fetch ("https://rickoungeli.github.io/ngelinsabakarickou_5_18052021/api/teddies")
+//J'envoie la reqûete (fetch) stockée dans la variable "reqFetch" et j'attend les résultats "response"
+//Je reçois une reponse en forme de promesse, je la convetie en objet JSON appelé 'datas'
+
 const reqFetch = async() => {
-    datas = await fetch ("http://localhost:5500/api/teddies") //J'envoie la reqûete et j'attend les résultats
-    .then(response => response.json()) //Lorsque je reçois une reponse (en forme de promesse), je la convetie en objet JSON appelé 'datas'
+    datas = await fetch ("http://localhost:5500/api/teddies") 
+    .then(response => response.json()) 
     .catch (err => {alert (err)})
 }
 
-//Affichage des données
+//Je crée une fonction "afficherData" qui permet d'afficher les données
 const afficherData = async() => {
     await reqFetch()
     
@@ -20,50 +24,30 @@ const afficherData = async() => {
             ))
             .map(data => ( //Traitement de chaque élément (data) de l'objet datas
             `
-                <a class='Card' href='ficheproduit.html'>
-                    <div class='photo'>
+                <a class='row Card p-2' href='ficheproduit.html?id=${data._id}'>
+                    <div class='col col-xs-12 col-sm-4 photo'>
                         <img src="${data.imageUrl}">
                     </div>
-                    <div class='descry'>
+                    <div class='col col-xs-12 col-sm-6 descry'>
                         <p class='nom'>${data.name}</p>
                         <p class='description'>${data.description}</p>
-                        <div class='prix'>
-                            <p>${data.price}</p>
-                            <p>€</p>
-                            <p style='visibility:none'>${data._id}</p>
-                        </div>
                     </div>
+                    <div class='col col-xs-12 col-sm-2 prix'><p>${separerLesMilliers(data.price)} €</p></div>
                 </a>
             `
             )).join('')
     )
 }
 afficherData()
-    /*
-    for (let i=0; i<data.length; i++) {
-        //Je récupère les données contenues dans l'objet datas
-        let id = datas[i]._id ;
-        let nom = datas[i].name;
-        let price = datas[i].price;
-        let couleurs = datas[i].colors;   
-        let imageUrl = datas[i].imageUrl        
-        let description1 = datas[i].description;
-        let description
-        if (description1.length>50) {
-            description = description1.substring(0,80) + '(...)'
-           if(document.body.clientWidth<470) {description = (description1.substring(0,50) + '(...)')}
-           if((document.body.clientWidth>=470) && (description1.length>=320)) {description = (description1.substring(0,150) + '(...)')}
-        } 
-        else {description = description1} 
 
-        
-        //Dans le card, j'ajoute le code ci-après :
-        card.innerHTML = "<div class='photo'><img src='"+imageUrl+"'></div><div class='descry'><p class='nom'>"+nom+"</p><p class='description'>"+description+"</p><div class='prix'><p>"+price+"</p><p>€</p></div></div>"       
-    }
+//Fonction recherche produits
 
-}
-*/
+searchZone.addEventListener('input', (e) => {
+    searchTerm = e.target.value //Je récupèrevtout ce qui est tapé dans l'input
+    afficherData()
+})
 
-
-    
-    
+//Fonction pour séparer les milliers dans le champs prix
+function separerLesMilliers(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g,'.')
+}    
---
+++ script2.js
@@ -0,0 +1,54 @@
+// -----------P A G E   2 ---------------//
+
+
+
+
+
+//Je récupère l'ID contenu dans l'URL
+let id = (document.location.search).substring(4)
+let searchTerm = ''
+
+
+//J'envoie la reqûete et j'interprete les résultats
+//fetch (`http://localhost:5500/api/teddies?_id=5be9c8541c9d440000665243`) 
+fetch (`http://localhost:5500/api/teddies?_id=${id}`) 
+//fetch ("https://geo.api.gouv.fr/communes?codePostal=94190")
+.then (response => response.json()
+.then(response2 => {
+    console.table(response2)
+
+
+    
+    let conteneur = document.getElementById('conteneurProduits')
+    for (let i=0; i<response2.length; i++) {
+        if(response2[i]._id===id){
+        //Je récupère les couleurs dans une variable
+            let couleurs = response2[i].colors;   
+            
+            conteneur.innerHTML = 
+            `
+                <div class='row Card p-2'>
+                    <div class='photo'>
+                        <img src=${response2[i].imageUrl}>
+                    </div>
+                    <div class='descry'>
+                        <p class='nom'>${response2[i].name}</p>
+                        <p class='description'>${response2[i].description}</p>
+                    </div>
+                    <div class='prix'>
+                        <p>${separerLesMilliers(response2[i].price)} €</p>
+                    </div>
+                </div>
+
+            `   
+        }    
+    }
+
+}))
+
+.catch(error => alert ("Erreur : " + error))
+
+//Fonction pour séparer les milliers dans le champs prix
+function separerLesMilliers(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g,'.')
+}   
