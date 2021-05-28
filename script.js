af1217977c81a7f7e89807f0c0415aefaef0a2d6
Rickou NGELI NSABAKA <rickoungeli@gmail.com>
2021-05-28 23:01:08 +0200

    ---

--- script.js
+++ script.js
@@ -2,17 +2,22 @@
 const conteneur = document.getElementById('conteneur')
 const searchZone = document.getElementById('searchzone')
 let page = (document.location.search).substring(7,6)
+let id = ''
 let searchTerm = ''
 let nombreDeProduits=0
-//Pour toutes les pages
-if(localStorage.getItem("nombreDeProduits")==null){
-    document.querySelector(".nombre-panier").innerHTML=nombreDeProduits
-}
-else {
-    AfficherNombreProduits()
+
+class produits {
+    constructor(id, name, description){
+        this.id = id
+        this.name = name
+        this.description = description
+    }
 }
 
-//fetch ("https://rickoungeli.github.io/ngelinsabakarickou_5_18052021/api/teddies")
+//Pour toutes les pages
+AfficherNombreProduits()
+
+
 //J'envoie la reqûete (fetch) stockée dans la variable "reqFetch" et j'attend les résultats "response"
 //Je reçois une reponse en forme de promesse, je la convetie en objet JSON appelé 'datas'
 if (page==''){
@@ -22,13 +27,11 @@ if (page==''){
         .then(response => response.json()) 
         .catch (err => {alert (err)})
     }
-
-    //Je crée une fonction "afficherData" qui permet d'afficher les données
+    //Fonction pour afficher tous les produits
     const afficherData = async() => {
         await reqFetch()
         
         conteneur.innerHTML = (
-
             datas
             .filter(data => data.name.toLowerCase().includes(searchTerm.toLowerCase()
                 ))
@@ -48,31 +51,25 @@ if (page==''){
                 )).join('')
         )
     }
+    //J'affiche les données
     afficherData()
 }
+
 if (page==2){
-    let id = (document.location.search).substring(11,39)
+    id = (document.location.search).substring(11,39)
     let btnAjoutPanier = document.getElementById('ajoutPanier')
 
     //J'envoie la reqûete et j'interprete les résultats
-    //fetch ("https://geo.api.gouv.fr/communes?codePostal=94190")
-    //fetch (`http://localhost:5500/api/teddies?_id=5be9c8541c9d440000665243`) 
     fetch (`http://localhost:5500/api/teddies/${id}`) 
     .then (response => response.json()
     .then(teddy => {
-        console.table(teddy)
-    //Je récupère les couleurs dans une variable
+        //Je récupère les couleurs dans une variable
         let couleurs =teddy .colors;   
+        afficheTeddy(teddy)  
         
-        afficheTeddy(teddy)   
-            
-
     }))
     .catch(error => alert ("Erreur : " + error))
-
-     //Ajout des produits au panier
-     AjoutPanier()
-     
+    ajoutPanier()
 }
 
 if (page=='3'){
@@ -81,7 +78,6 @@ if (page=='3'){
     
     
     `
-   
 }
 
 //Fonction recherche produits
@@ -90,23 +86,29 @@ searchZone.addEventListener('input', (e) => {
     afficherData()
 })
 
+
+//Fonction pour afficher la fiche d'un produit
 function afficheTeddy(teddy) {
     conteneur.innerHTML =
-        `
-                    <div class='row Card p-2'>
-                        <div class='photo'>
-                            <img src=${teddy.imageUrl}>
-                        </div>
-                        <div class='descry'>
-                            <p class='nom'>${teddy.name}</p>
-                            <p class='description'>${teddy.description}</p>
-                        </div>
-                        <div class='prix'>
-                            <p>${separerLesMilliers(teddy.price)} €</p>
-                        </div>
-                    </div>
+    `
+        <div class='row Card p-2'>
+            <div class='photo'>
+                <img src=${teddy.imageUrl}>
+            </div>
+            <div class='descry'>
+                <p class='nom'>${teddy.name}</p>
+                <p class='description'>${teddy.description}</p>
+            </div>
+            <div class='prix'>
+                <p>${separerLesMilliers(teddy.price)} €</p>
+            </div>
+        </div>
 
-                `
+    `
+    this.id = id
+    this.name = teddy.name
+    this.imageUrl = ''
+    this.description = teddy.description
 }
 
 //Fonction pour séparer les milliers dans le champs prix
@@ -114,18 +116,30 @@ function separerLesMilliers(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g,'.')
 }    
 
-//Fonction pour afficher le nombre de produits 
+//Fonction pour afficher le nombre de produits contenus dans le panier
 function AfficherNombreProduits() {
     nombreDeProduits = localStorage.length
     document.querySelector(".nombre-panier").innerHTML = nombreDeProduits
 }
 
 //Fonction pour ajouter des produits au panier
-function AjoutPanier() {
+function ajoutPanier() {
     
     btnAjoutPanier.addEventListener('click', (e) => {
-        localStorage.setItem("price",document.querySelector(".prix").innerText)
-        localStorage.setItem("names",document.querySelector(".nom").innerText)
-        AfficherNombreProduits()
+        if(localStorage.getItem('produit')){
+            let produit = localStorage.getItem('produit')
+            
+            let produit1 = new produits(this.id, this.name, this.description)
+            produit.push(produit1)
+            alert(produit)
+            localStorage.setItem("produit",JSON.stringify(produit))
+            
+            AfficherNombreProduits()
+        }
+        else {
+            let produit = new produits(this.id, this.name, this.description)
+            localStorage.setItem("produit",JSON.stringify(produit))
+        }
     })
+    
 }
